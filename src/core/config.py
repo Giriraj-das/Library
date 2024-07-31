056cff28e0451a591492d644f721458818981687
Giriraj-das <girirajdaspsdg@gmail.com>
2024-07-31 17:52:02 +0530

    add naming convention for models

--- src/core/config.py
+++ src/core/config.py
@@ -18,10 +18,18 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
+    naming_convention: dict[str, str] = {
+        'ix': 'ix_%(column_0_label)s',
+        'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
+        'ck': 'ck_%(table_name)s_%(constraint_name)s',
+        'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+        'pk': 'pk_%(table_name)s',
+    }
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=('.env.template', '.env'),  # next parameter overrides previous
+        env_file=('.env.template', '.env'),  # value of next parameter overrides value previous one
         case_sensitive=False,
         env_nested_delimiter='__',
         env_prefix='APP_CONFIG__',
