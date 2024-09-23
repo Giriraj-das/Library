1476b5cebcf29bb03e6324559f1e13dec113ed85
Oleksandr Radchenko <oradchenko108@gmail.com>
2024-09-23 08:33:27 +0530

    added title, description; changed imports ways

--- src/main.py
+++ src/main.py
@@ -4,24 +4,22 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
-from api import router as api_router
-from core.config import settings
-from core.models import db_helper
+from views import router
+from config import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Startup
     yield
-    # Shutdown
-    await db_helper.dispose()
 
 
 main_app = FastAPI(
     lifespan=lifespan,
     default_response_class=ORJSONResponse,
+    title='Warehouse',
+    description='FastAPI, PostgreSQL, SQLAlchemy(v2), Docker, Pytest',
 )
-main_app.include_router(api_router)
+main_app.include_router(router)
 
 if __name__ == '__main__':
     uvicorn.run(
