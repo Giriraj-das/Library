e7de881ae13198b02b6522ce4064e5cbc1aaaf93
Oleksandr Radchenko <oradchenko108@gmail.com>
2024-09-24 18:02:21 +0530

    added TIMESTAMP to created_at field

--- src/models/order.py
+++ src/models/order.py
@@ -1,13 +1,12 @@
 import enum
 from datetime import datetime, timezone
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING
 
-from sqlalchemy import func, Enum
+from sqlalchemy import func, Enum, TIMESTAMP
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 from models import Base
 
 if TYPE_CHECKING:
-    from models import Product
     from models import OrderItem
 
 
@@ -24,10 +23,11 @@ class Order(Base):
         server_default=OrderStatus.in_process.value,
     )
     created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
         default=datetime.now(timezone.utc),
         server_default=func.now(),
     )
 
-    products_details: Mapped[List['OrderItem']] = relationship(
-        back_populates='order'
+    products_details: Mapped[list['OrderItem']] = relationship(
+        back_populates='order',
     )
